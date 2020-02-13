54dd98608c43cd16a1394a7dcac1445e00b5361a
ItO0o <k17014kk@aitech.ac.jp>
2020-02-13 18:15:32 +0900

    【追加】傾き

--- Beast_Hunt_Copy/Assets/Scripts/Player/PlayerMove.cs
+++ Beast_Hunt_Copy/Assets/Scripts/Player/PlayerMove.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 //ito
 //    プレイヤーの動きを制御するメソッド
@@ -6,8 +8,7 @@
 //    矢印で平行移動
 //ジャンプメソッド追加予定
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMove : MonoBehaviour {
     public static bool moveFlag = false;
 
     private float z;
@@ -19,6 +20,9 @@ public class PlayerMove : MonoBehaviour
     private GameObject pointer;
 
     private Rigidbody player;
+    private GameObject cg;
+    private List<GameObject> cgAll;
+    
 
     [SerializeField]
     public float maxSpeed;
@@ -38,29 +42,44 @@ public class PlayerMove : MonoBehaviour
 
     GameObject runParticle;
 
+    float rotationCos;
+
     public enum Action {
         non,
         Run,
         Walk,
         Jump,
-        Attack
+        Attack,
+        FallDown
     }
-    
-    private void Start()
-    {
+
+    private void Start() {
         player = this.GetComponent<Rigidbody>();
         pointer = GameObject.Find("Pointer");
         key = new bool[keyCount];
         ps = this.GetComponent<PlayerStatus>();
         runParticle = GameObject.Find("Run_Particle");
+        cg = GameObject.Find("CG");
+        cgAll = new List<GameObject>();
+        GameObject temp = GameObject.Find("CG");
+        //cgAll.Add(temp);
+        GetAllCGObj(temp);
     }
 
     private void Update() {
+
+        //Debug.Log(currentAction);
+
         z = Input.GetAxis("MoveHorizontal");
         x = Input.GetAxis("MoveVertical");
 
-        Jump();
-        RunEffect();
+        rotationCos = Vector3.Dot(new Vector3(moveVector.x, 0, moveVector.z), new Vector3(player.transform.right.x, 0, player.transform.right.z));
+
+        if (currentAction != Action.FallDown) {
+            Jump();
+            RunEffect();
+        }
+
 
         if (Input.GetKeyDown(KeyCode.F)) {
             this.GetComponent<Animator>().SetTrigger("Attack");
@@ -68,24 +87,33 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         cameraHolizontal = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
         moveVector = (cameraHolizontal * x + Camera.main.transform.right * z).normalized;
 
         moveVector = Vector3.Lerp(tempVector, moveVector, acceleration);
-        
+
         if (this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("WL_Attack_Bite_Left") || ps.playerStatus == PlayerStatus.Status.Stan) {
             Stop();
             return;
         }
 
-        player.velocity = new Vector3(moveVector.x  * maxSpeed, player.velocity.y, moveVector.z * maxSpeed);
+        player.velocity = new Vector3(moveVector.x * maxSpeed, player.velocity.y, moveVector.z * maxSpeed);
 
-        PlayerRotation();
-        PlayerUpdatePostion();
+        if (currentAction != Action.FallDown) {
+            PlayerRotation();
+            PlayerUpdatePostion();
+        }
+
+        RecoveryRotation();
 
         tempVector = moveVector;
+    }
+
+    private void LateUpdate() {
+        //center.transform.eulerAngles = new Vector3(0,center.transform.eulerAngles.y,0);
+        //cg.transform.Rotate(90, 0, 0);
+        RunRotation();
     }
 
     void Jump() {
@@ -103,7 +131,6 @@ public class PlayerMove : MonoBehaviour
     }
 
     void PlayerRotation() {
-        float rotationCos = Vector3.Dot(new Vector3(moveVector.x, 0, moveVector.z), new Vector3(player.transform.right.x, 0, player.transform.right.z));
         if (rotationCos != 0) {
             player.transform.Rotate(0, rotationCos * rotationSpeed, 0);
         }
@@ -141,5 +168,56 @@ public class PlayerMove : MonoBehaviour
             //runParticle.SetActive(false);
             runParticle.GetComponent<ParticleSystem>().maxParticles = 0;
         }
+    }
+
+    //float tempRotation;
+
+    void RunRotation() {
+        Debug.Log(rotationCos * 3600);
+        //cg.transform.Rotate(45, 0,0);
+        //cg.transform.Rotate(1,0,0);
+        if (currentAction == Action.Run) {
+            //if ((cg.transform.eulerAngles.z > 30 && cg.transform.eulerAngles.z < 180 && rotationCos > 0) ||
+            //    (cg.transform.eulerAngles.z > 180 && cg.transform.eulerAngles.z < 330 && rotationCos < 0) ||
+            //    (cg.transform.eulerAngles.z >= 0 && cg.transform.eulerAngles.z <= 30) || (cg.transform.eulerAngles.z >= 330 && player.transform.eulerAngles.z <= 360)) {
+            //cg.transform.eulerAngles = new Vector3(cg.transform.eulerAngles.x , cg.transform.eulerAngles.y + 30, cg.transform.eulerAngles.z);
+            if (rotationCos < 30 && rotationCos > -30) {
+                cg.transform.Rotate(rotationCos * 3600, 0, 0);
+            } else if(rotationCos > 30) {
+                cg.transform.Rotate(30, 0, 0);
+            }else if (rotationCos < -30) {
+                cg.transform.Rotate(-30, 0, 0);
+            }
+
+            //}
+            //cg.transform.eulerAngles = new Vector3(-90, cg.transform.eulerAngles.y , 90);
+        } else {
+            //if (cg.transform.eulerAngles.z >= 0 && cg.transform.eulerAngles.z< 90) {
+            //    cg.transform.Rotate(0, 0, -0.01f);
+            //} else if (cg.transform.eulerAngles.z > 270 && cg.transform.eulerAngles.z< 360) {
+            //    cg.transform.Rotate(0, 0, 0.01f);
+            //}
+        }
+        //tempRotation = rotationCos;
+    }
+
+    void RecoveryRotation() {
+        //Debug.Log(player.transform.eulerAngles);
+        if ((player.transform.eulerAngles.x > 90 && player.transform.eulerAngles.x < 270) || (player.transform.eulerAngles.z > 90 && player.transform.eulerAngles.z < 270)) {
+            currentAction = Action.FallDown;
+        }
+        if (currentAction == Action.FallDown) {
+            //if () {
+
+            //}
+        }
+    }
+
+    void GetAllCGObj(GameObject obj) {
+        for (int i = 0; i < obj.transform.childCount; i++) {
+            cgAll.Add(obj.transform.GetChild(i).gameObject);
+            GetAllCGObj(obj.transform.GetChild(i).gameObject);
+        }
+
     }
 }
--- Beast_Hunt_Copy/Assets/Scripts/World/SetTree.cs
+++ Beast_Hunt_Copy/Assets/Scripts/World/SetTree.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class SetTree : MonoBehaviour
 {
+    GameObject[] natureObj;
     [SerializeField]
     public int treeCount;
     // Start is called before the first frame update
     void Start()
     {
         GameObject trees = GameObject.Find("Trees");
+        natureObj = new GameObject[3];
+        natureObj[0] = Resources.Load<GameObject>("Fir_1");
+        natureObj[1] = Resources.Load<GameObject>("Fir_2");
+        natureObj[2] = Resources.Load<GameObject>("Grass_1");
+        //natureObj[3] = Instantiate(Resources.Load<GameObject>("Fir_1"), trees.transform);
         for (int i = 0; i < treeCount; i++)
         {
-            GameObject instObj = Instantiate(Resources.Load<GameObject>("Tree"), trees.transform);
-            instObj.name = "Tree_" + treeCount;
-            float randY = Random.Range(10f, 25f);
-            instObj.transform.localScale = new Vector3(Random.Range(12f, 15f), randY, Random.Range(10f, 15f));
-            instObj.transform.position = new Vector3(Random.Range(-50f, 50f), 50, Random.Range(-50f, 50f));
-            instObj.transform.rotation = Quaternion.Euler(0, Random.Range(0, 180), 0);
-            RaycastHit hit;
-            if (Physics.Raycast(instObj.transform.FindChild("Stump").transform.position, -this.transform.up, out hit,100,1))
-            {
-                instObj.transform.position -= new Vector3(0, hit.distance, 0);
+            int natureRand = Random.Range(0, 100);
+            GameObject instObj;
+            if (natureRand > 5) {
+                float max = 0.5f, min= 1f;
+                instObj = Instantiate(natureObj[2], trees.transform);
+                instObj.transform.localScale = new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
+                instObj.transform.position = new Vector3(Random.Range(-50f, 50f), 50, Random.Range(-50f, 50f));
+                instObj.transform.rotation = Quaternion.Euler(0, Random.Range(0, 180), 0);
+                instObj.name += treeCount;
+                RaycastHit hit;
+                if (Physics.Raycast(instObj.transform.position, -this.transform.up, out hit, 100, 1)) {
+                    instObj.transform.position -= new Vector3(0, hit.distance, 0);
+                }
+
+            } else if (natureRand <= 5) {
+                float randY = Random.Range(1f, 2f);
+                instObj = Instantiate(natureObj[Random.Range(0,2)], trees.transform);
+                instObj.transform.localScale = new Vector3(Random.Range(1f, 2f), randY, Random.Range(1f, 2f));
+                instObj.transform.position = new Vector3(Random.Range(-50f, 50f), 50, Random.Range(-50f, 50f));
+                instObj.transform.rotation = Quaternion.Euler(0, Random.Range(0, 180), 0);
+                instObj.name += treeCount;
+                RaycastHit hit;
+                if (Physics.Raycast(instObj.transform.position, -this.transform.up, out hit, 100, 1)) {
+                    instObj.transform.position -= new Vector3(0, hit.distance, 0);
+                }
             }
         }
     }
